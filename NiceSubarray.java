ed28159b687db86676d49276738779cbca281312
Rajashri Mali <malirajashree2005@gmail.com>
2026-05-17 21:08:04 +0530

    Sliding window pattern

---
+++ NiceSubarray.java
@@ -0,0 +1,31 @@
+class  NiceSubarray{
+
+    public int atMost(int[] nums, int k) {
+
+        int left = 0;
+        int count = 0;
+
+        for(int right = 0; right < nums.length; right++) {
+
+            if(nums[right] % 2 != 0)
+                k--;
+
+            while(k < 0) {
+
+                if(nums[left] % 2 != 0)
+                    k++;
+
+                left++;
+            }
+
+            count += right - left + 1;
+        }
+
+        return count;
+    }
+
+    public int numberOfSubarrays(int[] nums, int k) {
+
+        return atMost(nums, k) - atMost(nums, k - 1);
+    }
+}
